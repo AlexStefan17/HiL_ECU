6d670e6d655a067a448c7dd333697cca81153096
Alex Stefan <stefan.alex170299@gmail.com>
2024-05-27 19:35:03 +0300

    Modified compile sh

--- compile_upload.sh
+++ compile_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the base directory
-BASE_DIR="/home/alex/HiL_ECU"
+BASE_DIR="/var/lib/jenkins/workspace/git"
 
 # Define the paths for the sketches
 SLAVE_LED_PATH="$BASE_DIR/slave_led/slave_led.ino"
@@ -11,6 +11,9 @@ MASTER_LED_PATH="$BASE_DIR/master_led/master_led.ino"
 BOARD="arduino:avr:uno"
 SLAVE_PORT="/dev/ttyUSB1"
 MASTER_PORT="/dev/ttyUSB0"
+
+# Ensure PATH is set to include arduino-cli installation directory
+export PATH=$PATH:/var/lib/jenkins/workspace/git/bin
 
 # Compile and upload the slave_led sketch
 arduino-cli compile -b $BOARD $SLAVE_LED_PATH
