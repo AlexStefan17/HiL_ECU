4091cdba99f0c188641e19a8d3e32993df2d7bfc
Alex Stefan <stefan.alex170299@gmail.com>
2024-05-27 21:35:11 +0300

    added arduino_cli zip

--- compile_upload.sh
+++ compile_upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define the base directory
-BASE_DIR="/var/lib/jenkins/workspace"
+BASE_DIR="/var/lib/jenkins/workspace/git"
 
 # Define the paths for the sketches
-SLAVE_LED_PATH="$BASE_DIR/git/slave_led/slave_led.ino"
-MASTER_LED_PATH="$BASE_DIR/git/master_led/master_led.ino"
+SLAVE_LED_PATH="$BASE_DIR/slave_led/slave_led.ino"
+MASTER_LED_PATH="$BASE_DIR/master_led/master_led.ino"
 
 # Define the board and port parameters
 BOARD="arduino:avr:uno"
@@ -16,9 +16,9 @@ MASTER_PORT="/dev/ttyUSB0"
 # export PATH=$PATH:/var/lib/jenkins/workspace/git/bin
 
 # Compile and upload the slave_led sketch
-./arduino-cli compile -b $BOARD $SLAVE_LED_PATH
-./arduino-cli upload -b $BOARD -p $SLAVE_PORT $SLAVE_LED_PATH
+arduino-cli compile -b $BOARD $SLAVE_LED_PATH
+arduino-cli upload -b $BOARD -p $SLAVE_PORT $SLAVE_LED_PATH
 
 # Compile and upload the master_led sketch
-./arduino-cli compile -b $BOARD $MASTER_LED_PATH
-./arduino-cli upload -b $BOARD -p $MASTER_PORT $MASTER_LED_PATH
+arduino-cli compile -b $BOARD $MASTER_LED_PATH
+arduino-cli upload -b $BOARD -p $MASTER_PORT $MASTER_LED_PATH
