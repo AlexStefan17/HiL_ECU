82879de5bbea0e09e4569177bfad9478b296df93
Alex Stefan <stefan.alex170299@gmail.com>
2024-05-28 21:16:58 +0300

    update py script

--- monitor.py
+++ monitor.py
@@ -4,12 +4,12 @@ def monitor_and_log():
     try:
         # Start the arduino-cli monitor command
         process = subprocess.Popen(['./arduino-cli', 'monitor', '-p', '/dev/ttyUSB0'],
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1)
     except Exception as e:
         print("Error:", e)
         return
     
-    with open('log.txt', 'a') as log_file:  # Use 'a' for append mode
+    with open('/home/alex/jenkins/workspace/HiL_ECU/log.txt', 'a') as log_file:  # Use absolute path for the log file
         while True:
             line = process.stdout.readline()
             if line:
@@ -19,7 +19,7 @@ def monitor_and_log():
 
                 # Check for the end-of-tests signal
                 if "All tests completed" in line:
-                    print("All tests completed. Exiting...")
+                    print("All tests completed.")
                     process.terminate()
                     break
 
